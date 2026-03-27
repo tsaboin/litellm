63ecaa6877c809eceefeca3565786f2a391ec7cb
tsaboin <tsaboin@gmail.com>
2026-03-27 02:22:39 +0100

    Openrouter Autoload

--- docker/prod_entrypoint.sh
+++ docker/prod_entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# Run configuration scripts
+python3 openrouterconfig/writeconfig.py
+
 if [ "$SEPARATE_HEALTH_APP" = "1" ]; then
     export LITELLM_ARGS="$@"
     export SUPERVISORD_STOPWAITSECS="${SUPERVISORD_STOPWAITSECS:-3600}"
